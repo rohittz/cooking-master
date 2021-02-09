efa490ad3c1452bc8aa9f7661287365e2e59470c
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-02-09 23:50:02 +0600

    finished

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 // TODO#1 : read the searchbox text and show the item if found else show an messsgae
 // saying " Item not found"
 let apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let lookUpUrll = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 let searchBox = document.getElementById("searchbox");
 let searchBtn = document.getElementById("searchbtn");
 let recipeCardStack = document.getElementById("recepies-stack");
+let recipeDetails = document.getElementById("recipe-details");
+
+function appendRecipe(mealImage, strMeal){
+	let recipeDiv = document.createElement("div");
+	recipeDiv.className = "recipe";
+	recipeDiv.innerHTML = `
+			<div class = "aboutrecipeImg">
+				<img src = "${mealImage}" alt= "${strMeal}">
+			</div>
+			<div class = "aboutRecipe">
+				<h1> ${strMeal} </h1>
+				<ul class = "list-ingredients">
+				</ul>
+			</div>
+	`
+	recipeDetails.append(recipeDiv);
+
+}
+function showrecipe(data){
+	// parsing ingredients and each of their measurement
+	recipeDetails.innerHTML = "";
+	appendRecipe(data.meals[0].strMealThumb, data.meals[0].strMeal);
+	for (let i = 1; i <= 20; i++){
+		let measurement = data.meals[0]["strMeasure"+i];
+		let ingredient = data.meals[0]["strIngredient"+i];
+		if(ingredient == null || ingredient.length == 0)
+			break;
+		else{
+			let ingredMeasure = measurement + " " + ingredient;
+			let listIngredient = document.createElement("li");
+			listIngredient.innerHTML = `<i class="fas fa-check-circle"></i>${ingredMeasure}`;
+			document.getElementsByClassName("list-ingredients")[0].append(listIngredient);
+		}
+	}
+}
+function clickedAction(mealId){
+	document.getElementById(mealId).addEventListener("click", function(){
+		fetch(`${lookUpUrll}${mealId}`)
+			.then(rawdata => rawdata.json())
+			.then(data => showrecipe(data))
+			.catch(error => notFoundMess("API ERROR!"));
+	})
+}
 function showData(mealName, mealPic, mealId){
 	recipeCardStack.style.gridTemplateColumns =
 		"repeat(4, 1fr)";
@@ -19,27 +63,30 @@ function showData(mealName, mealPic, mealId){
 		</div>
 	`;
 	recipeCardStack.append(mealCard);
+	clickedAction(mealId);
 }
-function notFoundMess(){
-	let message = `
+function notFoundMess(message){
+	recipeDetails.innerHTML = "";
+	let messageToShow = `
 		<div class = "notfound">
-		<h1> RECIPE NOT FOUND!</h1>
+		<h1>${message}</h1>
 		<img src = "https://www.nicepng.com/png/detail/317-3174344_decorar-paredes-cocina-chef-chef-hat-chef-vector.png">
 		</div>
 	`;
 	recipeCardStack.style.gridTemplateColumns =
 		"1fr";
 	// no need to clear the view, change the innerHTML with message
-	recipeCardStack.innerHTML = message;
+	recipeCardStack.innerHTML = messageToShow;
 }
 function parseData(data){
 	let mealName, mealPic, mealId;
 	if(data.meals == null){
-		notFoundMess();
+		notFoundMess("RECIPE NOT FOUND!");
 	}
 	else{
 		// clearing the view
 		recipeCardStack.innerHTML = "";
+		recipeDetails.innerHTML = "";
 		data.meals.forEach(meal => {
 			mealName = meal.strMeal;
 			mealPic = meal.strMealThumb;
@@ -52,6 +99,8 @@ let searchItem= function(){
 	let searchTerm = searchBox.value;
 	fetch(`${apiUrl}${searchTerm}`)
 		.then(rawdata => rawdata.json())
-		.then(data => parseData(data));
+		.then(data => parseData(data))
+		.catch(error => notFoundMess("API ERROR!"));
 }
 searchBtn.addEventListener("click", searchItem);
+
